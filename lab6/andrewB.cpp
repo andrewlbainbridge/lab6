fdf8bbb300d60ca2d3e751f1f4dd95ce231f3ec9
Student <student@lab-315-006.cs.csubak.edu>
2018-03-02 14:55:43 -0800

    added my individual file

--- lab6/andrewB.cpp
+++ lab6/andrewB.cpp
@@ -1,6 +1,37 @@
 //andrew bainbridge
 #include "fonts.h"
+#include <GL/glx.h>
+
 void showName(Rect r,int x,  int c)
 {
         ggprint8b(&r, x, c, "Andrew Bainbridge");
+}
+
+void drawBox(int x, int y)
+{
+    	    static float angle = 0.0;
+    //Draw the ship    red grn blu
+            glColor3ub(255,100,10);
+            glPushMatrix();
+            glTranslatef(x,y,0);
+            glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	    glTranslatef(-50,-50,0);
+
+	    angle = angle + 2.5;
+            glBegin(GL_QUADS);
+           	 glVertex2i(0,0);
+           	 glVertex2i(0,100);
+           	 glVertex2i(100,100);
+           	 glVertex2i(100,0);
+            glEnd();
+           
+            
+	    Rect r;
+	    r.bot = 50;
+	    r.left = 50;
+	    r.center = 1;
+	     ggprint8b(&r, 16, 0x00ffffff, "Andrew Bainbridge");
+	     glPopMatrix();
+
+    
 }
--- lab6/asteroids.cpp
+++ lab6/asteroids.cpp
@@ -748,6 +748,8 @@ void render()
 	ggprint8b(&r, 16, 0x00ffff00, "n asteroids destroyed: ");
 	extern void showName(Rect r, int x, int c);
 	showName(r,100,100);
+	extern void drawBox(int,int);
+	drawBox(gl.xres/2,gl.yres/2);
 	//
 	//-------------
 	//Draw the ship
